e21032bd6759a09ad4230fa8799ab089d5d9cc9b
sazaghi <zaaghisaad@gmail.com>
2024-11-04 19:49:16 +0700

    praktikum5

---
+++ locustest.py
@@ -0,0 +1,34 @@
+from locust import HttpUser, task, between
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 5)
+    @task
+    def get_all_users(self):
+        self.client.get("/users")
+    
+    @task
+    def get_user_by_id(self):
+        user_id = 1 
+        self.client.get(f"/users/{user_id}")
+    
+    @task
+    def create_user(self):
+        payload = {
+            "name": "test",
+            "email": "a@a"
+        }
+        self.client.post("/users", json=payload)
+    
+    @task
+    def update_user_by_id(self):
+        user_id = 1  # Randomized example user ID
+        payload = {
+            "name": "update test",
+            "email": "a@a"
+        }
+        self.client.put(f"/users/{user_id}", json=payload)
+    
+    @task
+    def delete_user_by_id(self):
+        user_id = 1  # Randomized example user ID
+        self.client.delete(f"/users/{user_id}")
